137d7aba9f1241cd91a076cfdce154c24c1099fd
nadiamuevog <43435245+nadiamuevog@users.noreply.github.com>
2019-08-08 12:37:39 +0200

    passt

--- icacyc_som.py
+++ icacyc_som.py
@@ -7,8 +7,7 @@ plt.ion()
 base_dir ="C:/Users/muellena/Desktop/Desktop/Experiment Somatosensorik/SOMA Daten/"
 proc_dir = base_dir+"proc/"
 
-subjs = ["SOM_12","SOM_13","SOM_15","SOM_17","SOM_18","SOM_19","SOM_20","SOM_21","SOM_22","SOM_23","SOM_24","SOM_26","SOM_27","SOM_29","SOM_30","SOM_31","SOM_32","SOM_33","SOM_34","SOM_35","SOM_36"]
-subjs = ["SOM_10","SOM_16"]
+subjs = ["SOM_10","SOM_12","SOM_13","SOM_15","SOM_16","SOM_17","SOM_18","SOM_19","SOM_20","SOM_21","SOM_22","SOM_23","SOM_24","SOM_26","SOM_27","SOM_29","SOM_30","SOM_31","SOM_32","SOM_33","SOM_34","SOM_35","SOM_36"]
 runs = ["2"]
 
 filelist = []
@@ -27,26 +26,28 @@ class Cycler():
         self.filelist = filelist
         self.ref_comp_num = ref_comp_num
 
-#def go(self,idx=0):
-    def go(self):
+    def go(self,idx=0):
         plt.close('all')
         # load the next raw/ICA files
-        self.fn = self.filelist.pop()
+        self.fn = self.filelist.pop(idx)
         self.epo = mne.read_epochs(self.fn[0],preload=True)
         self.ica = mne.preprocessing.read_ica(self.fn[3])
-        self.ica_ref = mne.preprocessing.read_ica(self.fn[1])
+        self.icaref = mne.preprocessing.read_ica(self.fn[1])
+        self.icameg = mne.preprocessing.read_ica(self.fn[2])
 
         # housekeeping on reference components, add them to raw data
-        #refcomps = self.icaref.get_sources(self.epo)
-        #for c in refcomps.ch_names[:self.ref_comp_num]: # they need to have REF_ prefix to be recognised by MNE algorithm
-        #    refcomps.rename_channels({c:"REF_"+c})
-        #self.epo.add_channels([refcomps])
+        refcomps = self.icaref.get_sources(self.epo)
+        for c in refcomps.ch_names[:self.ref_comp_num]: # they need to have REF_ prefix to be recognised by MNE algorithm
+            refcomps.rename_channels({c:"REF_"+c})
+        self.epo.add_channels([refcomps])
 
         self.comps = []
 
         # plot everything out for overview
-        self.ica.plot_components(picks=list(range(20)))
-        self.ica.plot_sources(self.epo)
+        self.ica.plot_components(picks=list(range(40)))
+        self.ica.plot_sources(self.epo, stop=8)
+        self.epo.plot(n_channels=64,n_epochs=8,scalings=dict(mag=2e-12,ref_meg=3e-12,misc=10))
+        self.epo.plot_psd(fmax=60,average=False,bandwidth=0.8)
 
         #Fenster um ICA Komponenten auszuwählen kann immer mit cyc.ica.plot_sources(cyc.epo) zurückgeholt werden
 
@@ -59,7 +60,7 @@ class Cycler():
     def show_file(self):
         print("Current raw file: "+self.fn[0])
 
-    def without(self,comps=None,fmax=40):
+    def without(self,comps=None,fmax=60):
         # see what the data would look like if we took comps out
         self.comps +=self.ica.exclude
         if not comps:
@@ -67,11 +68,11 @@ class Cycler():
         test = self.epo.copy()
         test.load_data()
         test = self.ica.apply(test,exclude=comps)
-        test.plot_psd(fmax=fmax)
-        test.plot(n_epochs=12,n_channels=30,scalings=dict(mag=2e-12))
+        test.plot_psd(fmax=fmax,average=False,bandwidth=0.8)
+        test.plot(n_epochs=8,n_channels=64,scalings=dict(mag=2e-12,ref_meg=3e-12,misc=10))
         self.test = test
 
-    def identify_bad(self,method,threshold=4):
+    def identify_bad(self,method,threshold=0.5):
         # search for components which correlate with noise
         # example: method= ["ecg","ref"]
         if isinstance(method,str):
@@ -81,14 +82,15 @@ class Cycler():
         for meth in method:
             print(meth)
             if meth == "eog":
-                func = self.ica.find_bads_eog
+                inds, scores = self.ica.find_bads_eog(self.epo)
             elif meth == "ecg":
-                func = self.ica.find_bads_ecg
+                inds, scores = self.ica.find_bads_ecg(self.epo)
             elif meth == "ref":
-                func = self.ica.find_bads_ref
+                inds, scores = self.ica.find_bads_ref(self.epo, method="separate",
+                                                      threshold=threshold,
+                                                      bad_measure="cor")
             else:
                 raise ValueError("Unrecognised method.")
-            inds, scores = func(self.epo)
             print(inds)
             if inds:
                 self.ica.plot_scores(scores, exclude=inds)
